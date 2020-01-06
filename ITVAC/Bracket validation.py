43ebe64f336a85bc09e7c95ea9e358beb350d6b3
ABHINAVKISHORE <45748742+kishorebolt03@users.noreply.github.com>
2020-01-06 14:19:50 +0530

    Create Bracket validation.py

---
+++ ITVAC/Bracket validation.py
@@ -0,0 +1,18 @@
+d={'{':'}','(':')','[':']'}
+n=list(input())
+l=[]
+f=[]
+for i in n:
+    if(i in d.keys()):
+        l.append(i)
+    else:
+        if(l!=[]):
+            if d[l[-1]]==i:
+                l.pop()
+        else:
+            f.append(1)
+
+if(f==[]):
+    print('True')
+else:
+    print('False')
